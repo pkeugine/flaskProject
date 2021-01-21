cbd888914d858edf9848a15bac31421b9cc9d6b4
Eugine Park <pkeugine@gmail.com>
2021-01-21 15:12:00 +0900

    feat(app.py): create end points for different features

--- app.py
+++ app.py
@@ -2,11 +2,51 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+	{
+		'name': 'My Wonderful Store',
+		'items': [
+			{
+				'name': 'My Item',
+				'price': 15.99
+			}
+		]
+	}
+]
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+# POST - used to receive data
+# GET - used to send data back only
+# THESE ARE SERVER PERSPECTIVE
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+	pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+	pass
+
+
+# GET /store
+@app.route('/store')
+def get_stores():
+	pass
+
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+	pass
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+	pass
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(port=5000)
